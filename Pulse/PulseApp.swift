d41245ae9879613e65182437c0d4e7eac60b6611
yuldshev <ikrom921@gmail.com>
2025-12-11 14:40:31 +0500

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by T on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
