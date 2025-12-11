34d2bd22c751c6fd0a37ff9e0c0f278d45e3dc5d
yuldshev <ikrom921@gmail.com>
2025-12-11 15:47:41 +0500

    Add data service with mock JSON

---
+++ Pulse/Services/WorkoutDataService.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+// MARK: - Protocol
+protocol WorkoutDataServiceProtocol {
+  func loadWorkouts() async throws -> [Workout]
+  func loadMetadata() async throws -> [String: WorkoutMetadata]
+  func loadDiagramData() async throws -> [String: WorkoutDiagramData]
+}
+
+// MARK: - Implementation
+final class WorkoutDataService: WorkoutDataServiceProtocol {
+  
+  enum DataError: Error {
+    case fileNotFound
+    case invalidJSON
+    case decodingFailed(Error)
+  }
+  
+  func loadWorkouts() async throws -> [Workout] {
+    let response: WorkoutListResponse = try await loadJSON(fileName: "list_workouts")
+    return response.data
+  }
+  
+  func loadMetadata() async throws -> [String: WorkoutMetadata] {
+    let response: MetadataResponse = try await loadJSON(fileName: "metadata")
+    return response.workouts
+  }
+  
+  func loadDiagramData() async throws -> [String: WorkoutDiagramData] {
+    let response: DiagramDataResponse = try await loadJSON(fileName: "diagram_data")
+    return response.workouts
+  }
+  
+  private func loadJSON<T: Decodable>(fileName: String) async throws -> T {
+    guard let url = Bundle.main.url(
+      forResource: fileName,
+      withExtension: "json",
+      subdirectory: "Resources/data"
+    ) else {
+      throw DataError.fileNotFound
+    }
+    
+    do {
+      let data = try Data(contentsOf: url)
+      let decoder = JSONDecoder()
+      return try decoder.decode(T.self, from: data)
+    } catch let decodingError as DecodingError {
+      throw DataError.decodingFailed(decodingError)
+    } catch {
+      throw DataError.invalidJSON
+    }
+  }
+}
+
